b7733dafda8ca46959f561bf3ef2d142efc147e0
phamviethoa <phamviethoa2416@gmail.com>
2025-06-17 13:31:18 +0700

    feat: add metrics for processor constructor

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -2,9 +2,9 @@ package com.example;
 
 import com.example.clickstream.config.AppConfig;
 import com.example.clickstream.processor.ClickstreamProcessor;
+import com.example.monitoring.metrics.ClickstreamMetrics;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.apache.spark.sql.SparkSession;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -19,13 +19,12 @@ public class Application {
 
         dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
-        // Start Spring Boot application
-
         try (ConfigurableApplicationContext context = new SpringApplicationBuilder(Application.class)
                 .run(args)) {
             SparkSession sparkSession = context.getBean(SparkSession.class);
             AppConfig appConfig = context.getBean(AppConfig.class);
-            ClickstreamProcessor processor = new ClickstreamProcessor(sparkSession, appConfig);
+            ClickstreamMetrics  metrics = context.getBean(ClickstreamMetrics.class);
+            ClickstreamProcessor processor = new ClickstreamProcessor(sparkSession, appConfig, metrics);
             processor.startProcessing();
 
         } catch (Exception e) {
